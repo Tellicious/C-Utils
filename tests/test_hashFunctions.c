7c46223a665bec5b41c95f5076c555c26b555baa
Tellicious <tellicious@icloud.com>
2024-11-01 18:45:27 +0100

    Added unit tests for `hashFunctions.c`

--- tests/test_hashFunctions.c
+++ tests/test_hashFunctions.c
@@ -40,76 +40,162 @@
 
 #include <cmocka.h>
 
+/* Support functions ---------------------------------------------------------*/
+
+static uint8_t skipAssert = 0;
+
+void ADVUtils_testAssert(const int result, const char* const expression, const char* const file, const int line) {
+    if (skipAssert) {
+        return;
+    } else {
+        mock_assert(result, expression, file, line);
+    }
+}
+
 /* Functions -----------------------------------------------------------------*/
 
-static void test_hash_FNV1A(void** state) {
-    (void)state; /* unused */
-
-    // Test known values
-    assert_int_equal(hash_FNV1A(""), 2166136261U);
-    assert_int_equal(hash_FNV1A("a"), 3826002220U);
-    assert_int_equal(hash_FNV1A("abc"), 1081548509U);
-    assert_int_equal(hash_FNV1A("message"), 2381907949U);
-
-    // Test edge case: long string
-    char long_str[1000];
-    for (int i = 0; i < 999; ++i) {
-        long_str[i] = 'a';
-    }
-    long_str[999] = '\0';
-    assert_int_not_equal(hash_FNV1A(long_str), 0);
-
-    // Test edge case: different case
-    assert_int_not_equal(hash_FNV1A("abc"), hash_FNV1A("ABC"));
+static void test_hash_FNV1A_basic(void** state) {
+    (void)state; // unused
+    assert_int_equal(hash_FNV1A("test"), 427567909);
 }
 
-static void test_hash_djb(void** state) {
-    (void)state; /* unused */
-
-    // Test known values
-    assert_int_equal(hash_djb(""), 5381U);
-    assert_int_equal(hash_djb("a"), 177670U);
-    assert_int_equal(hash_djb("abc"), 193485963U);
-    assert_int_equal(hash_djb("message"), 210718207996U);
-
-    // Test edge case: long string
-    char long_str[1000];
-    for (int i = 0; i < 999; ++i) {
-        long_str[i] = 'a';
-    }
-    long_str[999] = '\0';
-    assert_int_not_equal(hash_djb(long_str), 0);
-
-    // Test edge case: different case
-    assert_int_not_equal(hash_djb("abc"), hash_djb("ABC"));
+static void test_hash_FNV1A_empty_string(void** state) {
+    (void)state; // unused
+    assert_int_equal(hash_FNV1A(""), 2216829733);
 }
 
-static void test_hash_sdbm(void** state) {
-    (void)state; /* unused */
+static void test_hash_FNV1A_null_pointer(void** state) {
+    (void)state; // unused
+    expect_assert_failure(hash_FNV1A(NULL));
+}
 
-    // Test known values
-    assert_int_equal(hash_sdbm(""), 0U);
-    assert_int_equal(hash_sdbm("a"), 97U);
-    assert_int_equal(hash_sdbm("abc"), 96354U);
-    assert_int_equal(hash_sdbm("message"), 418229425U);
+static void test_hash_FNV1A_special_characters(void** state) {
+    (void)state; // unused
+    assert_int_equal(hash_FNV1A("!@#$%^&*()"), 114978727);
+}
 
-    // Test edge case: long string
-    char long_str[1000];
-    for (int i = 0; i < 999; ++i) {
-        long_str[i] = 'a';
-    }
-    long_str[999] = '\0';
-    assert_int_not_equal(hash_sdbm(long_str), 0);
+static void test_hash_FNV1A_different_cases(void** state) {
+    (void)state; // unused
+    assert_int_not_equal(hash_FNV1A("test"), hash_FNV1A("TEST"));
+    assert_int_not_equal(hash_FNV1A("Test"), hash_FNV1A("tEsT"));
+}
 
-    // Test edge case: different case
-    assert_int_not_equal(hash_sdbm("abc"), hash_sdbm("ABC"));
+static void test_hash_FNV1A_long_string(void** state) {
+    (void)state; // unused
+    char* long_string = "This is a very long string to test the hash function for long inputs.";
+    assert_int_equal(hash_FNV1A(long_string), 70682599);
+}
+
+static void test_hash_FNV1A_numeric_string(void** state) {
+    (void)state; // unused
+    assert_int_equal(hash_FNV1A("1234567890"), 3402609060);
+}
+
+// Test cases for hash_djb
+static void test_hash_djb_basic(void** state) {
+    (void)state; // unused
+    assert_int_equal(hash_djb("test"), 2090756197);
+}
+
+static void test_hash_djb_empty_string(void** state) {
+    (void)state; // unused
+    assert_int_equal(hash_djb(""), 5381);
+}
+
+static void test_hash_djb_null_pointer(void** state) {
+    (void)state; // unused
+    expect_assert_failure(hash_djb(NULL));
+}
+
+static void test_hash_djb_special_characters(void** state) {
+    (void)state; // unused
+    assert_int_equal(hash_djb("!@#$%^&*()"), 1465968561);
+}
+
+static void test_hash_djb_different_cases(void** state) {
+    (void)state; // unused
+    assert_int_not_equal(hash_djb("test"), hash_djb("TEST"));
+    assert_int_not_equal(hash_djb("Test"), hash_djb("tEsT"));
+}
+
+static void test_hash_djb_long_string(void** state) {
+    (void)state; // unused
+    char* long_string = "This is a very long string to test the hash function for long inputs.";
+    assert_int_equal(hash_djb(long_string), 2041601053);
+}
+
+static void test_hash_djb_numeric_string(void** state) {
+    (void)state; // unused
+    assert_int_equal(hash_djb("1234567890"), 4018482162);
+}
+
+// Test cases for hash_sdbm
+static void test_hash_sdbm_basic(void** state) {
+    (void)state; // unused
+    assert_int_equal(hash_sdbm("test"), 1195757874);
+}
+
+static void test_hash_sdbm_empty_string(void** state) {
+    (void)state; // unused
+    assert_int_equal(hash_sdbm(""), 0);
+}
+
+static void test_hash_sdbm_null_pointer(void** state) {
+    (void)state; // unused
+    expect_assert_failure(hash_sdbm(NULL));
+}
+
+static void test_hash_sdbm_special_characters(void** state) {
+    (void)state; // unused
+    assert_int_equal(hash_sdbm("!@#$%^&*()"), 773972894);
+}
+
+static void test_hash_sdbm_different_cases(void** state) {
+    (void)state; // unused
+    assert_int_not_equal(hash_sdbm("test"), hash_sdbm("TEST"));
+    assert_int_not_equal(hash_sdbm("Test"), hash_sdbm("tEsT"));
+}
+
+static void test_hash_sdbm_long_string(void** state) {
+    (void)state; // unused
+    char* long_string = "This is a very long string to test the hash function for long inputs.";
+    assert_int_equal(hash_sdbm(long_string), 3092146248);
+}
+
+static void test_hash_sdbm_numeric_string(void** state) {
+    (void)state; // unused
+    assert_int_equal(hash_sdbm("1234567890"), 800103739);
 }
 
 int main(void) {
     const struct CMUnitTest test_hashFunctions[] = {
-        cmocka_unit_test(test_hash_FNV1A),
-        cmocka_unit_test(test_hash_djb),
-        cmocka_unit_test(test_hash_sdbm),
+        // hash_FNV1A tests
+        cmocka_unit_test(test_hash_FNV1A_basic),
+        cmocka_unit_test(test_hash_FNV1A_empty_string),
+        cmocka_unit_test(test_hash_FNV1A_null_pointer),
+        cmocka_unit_test(test_hash_FNV1A_special_characters),
+        cmocka_unit_test(test_hash_FNV1A_different_cases),
+        cmocka_unit_test(test_hash_FNV1A_long_string),
+        cmocka_unit_test(test_hash_FNV1A_numeric_string),
+
+        // hash_djb tests
+        cmocka_unit_test(test_hash_djb_basic),
+        cmocka_unit_test(test_hash_djb_empty_string),
+        cmocka_unit_test(test_hash_djb_null_pointer),
+        cmocka_unit_test(test_hash_djb_special_characters),
+        cmocka_unit_test(test_hash_djb_different_cases),
+        cmocka_unit_test(test_hash_djb_long_string),
+        cmocka_unit_test(test_hash_djb_numeric_string),
+
+        // hash_sdbm tests
+        cmocka_unit_test(test_hash_sdbm_basic),
+        cmocka_unit_test(test_hash_sdbm_empty_string),
+        cmocka_unit_test(test_hash_sdbm_null_pointer),
+        cmocka_unit_test(test_hash_sdbm_special_characters),
+        cmocka_unit_test(test_hash_sdbm_different_cases),
+        cmocka_unit_test(test_hash_sdbm_long_string),
+        cmocka_unit_test(test_hash_sdbm_numeric_string),
     };
+
     return cmocka_run_group_tests(test_hashFunctions, NULL, NULL);
 }
